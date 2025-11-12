51169859b81374e3d5adad7b7a52af7e93987c81
Dreine Antonio <dreineantonio@hotmail.com>
2025-11-11 21:27:37 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/dreinedev/workshopmongo/services/UserService.java
+++ src/main/java/com/dreinedev/workshopmongo/services/UserService.java
@@ -32,8 +32,14 @@ public class UserService {
 		return repository.insert(obj);
 
 	}
-	
+
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
+
 }
