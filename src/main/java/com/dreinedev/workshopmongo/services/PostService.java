ade8bae170c049f157d198d7665b556fb6973ddb
Dreine Antonio <dreineantonio@hotmail.com>
2025-11-18 21:04:20 -0300

    Consulta simples com query methods

--- src/main/java/com/dreinedev/workshopmongo/services/PostService.java
+++ src/main/java/com/dreinedev/workshopmongo/services/PostService.java
@@ -1,19 +1,13 @@
 package com.dreinedev.workshopmongo.services;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
-
-import javax.swing.text.html.parser.Entity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dreinedev.workshopmongo.domain.Post;
-import com.dreinedev.workshopmongo.domain.User;
-import com.dreinedev.workshopmongo.dto.UserDTO;
 import com.dreinedev.workshopmongo.repository.PostRepository;
-import com.dreinedev.workshopmongo.repository.UserRepository;
 import com.dreinedev.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
@@ -25,5 +19,9 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
 	}
 }
